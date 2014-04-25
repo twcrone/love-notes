807a0362be91bde967d39bc8c67cd53baa4a24c3
Todd Crone <todd.crone@perceptivesoftware.com>
2014-04-24 23:54:54 -0400

    Fix LoveNoteSenderTest.

--- Rakefile
+++ Rakefile
@@ -4,3 +4,13 @@
 require File.expand_path('../config/application', __FILE__)
 
 Rails.application.load_tasks
+
+begin
+  require "rspec/core/rake_task"
+  desc "Run all examples"
+  RSpec::Core::RakeTask.new(:spec) do |t|
+    t.rspec_opts = %w[--color]
+    t.pattern = 'spec/**/*_spec.rb'
+  end
+rescue LoadError
+end
--- test/mailers/love_note_sender_test.rb
+++ test/mailers/love_note_sender_test.rb
@@ -2,11 +2,12 @@ require 'test_helper'
 
 class LoveNoteSenderTest < ActionMailer::TestCase
   test "send_text_message" do
-    mail = LoveNoteSender.send_text_message
-    assert_equal "Send text message", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    note = Note.new(message: 'Hi baby!')
+    mail = LoveNoteSender.send_text_message(note)
+    assert_equal 'Love Note', mail.subject
+    assert_equal ["8593613596@vtext.com", "tkcrone@gmail.com", "8593611777@vtext.com", "twcrone@gmail.com"], mail.to
+    assert_equal ["bigdaddy@ilovetheresacrone.com"], mail.from
+    assert_match "Hi baby!", mail.body.encoded
   end
 
 end
